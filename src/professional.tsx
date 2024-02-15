801bab0ef99a9d69318d3d2ae50064bf5391346b
Dillon-Goicoechea <77130346+Dillon-Goicoechea@users.noreply.github.com>
2024-02-14 22:15:27 -0800

    w

--- src/professional.tsx
+++ src/professional.tsx
@@ -2,7 +2,7 @@
 function ProfWork(){
     return(
         <>
-        <iframe src="/Dillon-Goicoechea.github.io/src/assets/Dillon_Goicoechea_Resume.pdf" height="500px" width="100%"/>
+        <iframe src="https://github.com/Dillon-Goicoechea/Dillon-Goicoechea.github.io/blob/ca26c9c529cec22cd3a668dbae7e554198dfb63e/src/assets/Dillon_Goicoechea_Resume.pdf" height="500px" width="100%"/>
         </>
     )
 }
