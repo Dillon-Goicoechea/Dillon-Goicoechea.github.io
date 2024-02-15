9923af4bb758bede0991b7aff1f19af46f4d7889
Dillon-Goicoechea <77130346+Dillon-Goicoechea@users.noreply.github.com>
2024-02-14 23:20:31 -0800

    qqq

--- src/professional.tsx
+++ src/professional.tsx
@@ -3,7 +3,7 @@
 function ProfWork(){
     return(
         <>
-        <embed src="dillon-goicoechea.github.io/src/assets/DillonGoicoecheaResume.pdf" height="500px" width="100%"/>
+        <div>In Progress</div>
         </>
     )
 }
